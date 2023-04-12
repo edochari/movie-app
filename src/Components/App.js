21ee09ba0a010676763c2cb278eb9bc1b3cd5465
hari krishna <hari.jsdeveloper@gmail.com>
2023-04-12 18:10:31 +0530

    adding reducers

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { data } from '../data';
 
+import MovieCard from './MovieCard';
+import Navbar from './Navbar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Navbar />
+       <div className='main'>
+        <div className='tabs'>
+          <div className='tab'>MOVIES</div>
+          <div className='tab'>FAVOURITES</div>
+        </div>
+        {data.map((movie)=>(
+         <MovieCard movie={movie} />
+        ))}
+       </div>
     </div>
   );
 }
